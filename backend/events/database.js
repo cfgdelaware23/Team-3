0e634ecdbdf72ebdb520d37f1581078d16497b52
Jason Ren <jasoncren22@gmail.com>
2023-09-29 18:39:03 -0400

    fixed database

--- backend/events/database.js
+++ backend/events/database.js
@@ -3,7 +3,7 @@ const { getDB } = require('../database');
 const getAllEvents = async () => {
   try {
     const db = await getDB();
-    const result = await db.collection('Users').findOne({ });
+    const result = await db.collection('Events').findOne({ });
     return result;
   } catch (err) {
     return err;
