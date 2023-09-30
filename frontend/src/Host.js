c4ab107ed5e0acc2dbdf44dda721f6b00c182d75
Vivian Yam <lovecheetahs101@gmail.com>
2023-09-30 03:00:52 -0400

    made cards for the events

--- frontend/src/Host.js
+++ frontend/src/Host.js
@@ -2,12 +2,35 @@ import React from 'react';
 import logo from './logo.png'
 import './Host.css'
 
+
+const Card = () => {
+    return (
+      <div className="card">
+        <img src="https://example.com/card-image.jpg" alt="Card Image" />
+        <div className="card-body">
+          <h2 className="card-title">Event</h2>
+          <p className="card-text">Description of Event</p>
+          <div className="card-buttons">
+            <button className="btn">View</button>
+            <button className="btn">Cancel</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
 const Host = () => {
     return (
         <>
         <div className="navbar">
             <img src={logo} alt="ACB Logo" className="logo" />
             <div className="right__side">
+                  <section class="section">
+                 <button class="add">Add Event</button>
+
+             </section>
+
+            
             <h2 className='user__name'>John Doe</h2>    
             
             </div>
@@ -17,29 +40,38 @@ const Host = () => {
         <div>
             
 
-             <section class="section">
-                 <button class="add">Add Event</button>
-
-             </section>
+           
 
              <section class="section">
                  <h1> My Upcoming Events</h1>
 
+
+
              </section>
 
-            
              <section class="section">
-                
-                 <button class="view">View</button>
 
-                 <button class="view">Cancel</button>
+                 <div className="card-container">
+                <Card />
+
+                <Card />
+                <Card />
+                
+                </div>
             </section>
+
 
              <section class="section">
 
                  <h1> Other Events</h1>
-                
-           </section>
+             </section>
+                 <section class="section">
+                 <Card />
+
+                 <Card />
+                 <Card />
+                 </section>
+     
 
              <footer className="footer">
                  <p className="text-footer">
@@ -57,55 +89,5 @@ const Host = () => {
     );
 }
 
-//  function Host() {
-//      return (
-//          <div>
-//             <div class="navbar background">
-//                 <ul class="navbar-tabs">
-//                      <div class="logo" >
-//                      <img src="./logo.png" alt="Logo" class="logo"></img>
-//                      </div>
-
-                    
-        
-//                  </ul>
-//                  <div class="rightbar">
-//                      <button class="username">Username</button>
-//                  </div>
-//              </div>
-
-//              <section class="section">
-//                  <button class="add">Add Event</button>
-
-//              </section>
-
-//              <section class="section">
-//                  <h1> My Upcoming Events</h1>
-
-//              </section>
-
-            
-//              <section class="section">
-//                  <button class="view">View</button>
-
-//                  <button class="view">Cancel</button>
-//             </section>
-
-//              <section class="section">
-
-//                  <h1> Other Events</h1>
-                
-//            </section>
-
-//              <footer className="footer">
-//                  <p className="text-footer">
-//                      JP Morgan Code for Good
-//                  </p>
-//              </footer>
-
-
-//          </div>
-//      )
-//  } 
 
 export default Host;
