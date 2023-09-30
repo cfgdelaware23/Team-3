888855a1f3f06de1286e1c4c17912aadb5ea3540
kulkvasu <kulkvasu@umich.edu>
2023-09-29 21:59:20 -0400

    Moved everything card related in App.js in EventCards.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,44 +4,21 @@ import './App.css';
 import Navbar from './Navbar';
 import OrganizationCard from './OrganizationCard';
 import Footer from './Footer';
+import EventCards from './EventCards';
 import Volunteers from './components/Volunteers/Volunteers';
 
 
-const dummyData = [
-    { id: 1, name: "Organization 1", description: "Description for Organization 1" },
-    { id: 2, name: "Organization 2", description: "Description for Organization 2" },
-    { id: 3, name: "Organization 3", description: "Description for Organization 3" },
-    // { id: 4, name}
-
-];
 
 
 function App() {
-    
-    const [organizations, setOrganizations] = useState(dummyData);
-
-    // NOTE: TRY FOR REAL DATA MAYBE???? DOUBLECHECK IF RIGHT
-    /*
-    useEffect(() => {
-        fetch('/organizations')
-            .then(response => response.json())
-            .then(data => setOrganizations(data))
-            .catch(error => console.error("Error fetching organizations:", error));
-    }, []); 
-    */
     return (
       <>
-        <div className="App">
-            <Navbar />
-            <div className="org-container">
-                {organizations.map(org => (
-                    <OrganizationCard key={org.id} name={org.name} description={org.description} />
-                ))}
-            </div>
-            
-        </div>
-        <Footer />
-        </>
+      <div className="App">
+          <Navbar />
+          <EventCards/>
+      </div>
+      <Footer />
+  </>
     );
 }
 
